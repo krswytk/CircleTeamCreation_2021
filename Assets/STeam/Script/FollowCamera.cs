b1fb88ad46e9cb1f808538e345e17baba6f7a214
1923036-PC\1923036 <s1923036@cco.kanagawa-it.ac.jp>
2021-02-10 23:08:35 +0900

    廊下

---
+++ Assets/STeam/Script/FollowCamera.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCamera : MonoBehaviour
+{
+
+    //寿司を格納する変数
+    public GameObject sushi;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        Vector3 sushiPos = sushi.transform.position;
+
+        //カメラとプレイヤーの位置を同じにする
+        transform.position = new Vector3(sushiPos.x, 0.8f, -3.3f);
+    }
+}
