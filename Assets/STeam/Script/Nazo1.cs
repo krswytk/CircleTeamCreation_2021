a600c8be374d7340143a94e7004c1e035cc2b455
1923036-PC\1923036 <s1923036@cco.kanagawa-it.ac.jp>
2021-02-26 11:28:04 +0900

    アイテムを使用して出る処理追加

---
+++ Assets/STeam/Script/Nazo1.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Nazo1 : NazoData
+{
+    [SerializeField]
+    GameObject g;
+
+    Menu m;
+    Item i;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        g.SetActive(false);
+        m = GameObject.FindWithTag("Player").GetComponent<Menu>();
+        i = GameObject.FindWithTag("Item").GetComponent<Item>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (right == true)
+            g.SetActive(true);
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.tag == "Player" && m.opcl == true)
+        {
+            if (i.getflag == true && i.itemkind == 0)
+            {
+                if (Input.GetKeyDown(KeyCode.Z))
+                {
+                    right = true;
+                }
+            }
+        }
+    }
+}
---
+++ Assets/STeam/Script/NazoData.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NazoData : MonoBehaviour
+{
+    //謎が解けたかどうか
+  public  bool right = false;
+
+   
+}
