5df92b3026fa14f9fb7a4e94a846ec766c9a8240
1923036-PC\1923036 <s1923036@cco.kanagawa-it.ac.jp>
2021-03-10 00:37:28 +0900

    色んなフラグ管理

--- Assets/STeam/Script/Warp.cs
+++ Assets/STeam/Script/Warp.cs
@@ -7,7 +7,8 @@ public class Warp : MonoBehaviour
 {
     SceneData scene;
 
-    public static bool inout=false;//教室内か外か static変数でどのシーンからも共通している
+    string scenename = "STeam/Scenes/";
+    //public static bool inout=false;//教室内か外か static変数でどのシーンからも共通している
 
     // Start is called before the first frame update
     void Start()
@@ -18,84 +19,28 @@ public class Warp : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
     }
     //プレイヤーがトリガーのオブジェクトに触れたら
     private void OnTriggerStay(Collider other)
     {
-        if (other.gameObject.name == "A-1")
+        if (other.gameObject.tag == "indoor")
         {
-            if (inout == false)
+            if (Input.GetKeyDown(KeyCode.Z))
             {
-                incord("A-1");
-            }
-            else
-            {
-                outcord();
+                scenename += other.gameObject.name;
+                SceneManager.sceneLoaded += scene.GameSceneLoaded;
+                SceneManager.LoadScene(scenename);
             }
         }
 
-        if (other.gameObject.name == "A-2")
+        if (other.gameObject.tag == "outdoor")
         {
-            if (inout == false)
+            if (Input.GetKeyDown(KeyCode.Z))
             {
-                incord("A-2");
+                SceneManager.sceneLoaded += scene.GameSceneLoaded;
+                SceneManager.LoadScene("STeam/Scenes/rouka");
             }
-            else
-            {
-                outcord();
-            }
-        }
-
-        if (other.gameObject.name == "B-1")
-        {
-            if (inout == false)
-            {
-                incord("B-1");
-            }
-            else
-            {
-                outcord();
-            }
-        }
-
-    }
-    //教室名を引数に取り、そのシーンをロード
-    public void incord(string s)
-    {
-        s = "STeam/Scenes/" + s;
-
-        if (Input.GetKeyDown(KeyCode.Z))
-        {
-            SceneManager.sceneLoaded += scene.GameSceneLoaded;
-
-            SceneManager.LoadScene(s);
-            inout = true;
         }
     }
-
-    //廊下に移動
-    public void outcord()
-    {
-
-        if (Input.GetKeyDown(KeyCode.Z))
-        {
-            SceneManager.sceneLoaded += scene.GameSceneLoaded;
-            SceneManager.LoadScene("STeam/Scenes/rouka");
-            inout = false;
-        }
-    }
-
 }
-/*if (other.gameObject.name == "教室の入り口のobjの名前")
-        {
-            if (inout == false)
-            {
-                incord("教室の名前");
-            }
-            else
-            {
-                outcord();
-            }
-        }
- */
