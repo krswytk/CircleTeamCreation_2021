e4656e288647419619828d575a598b7901a8acd8
douguya-imamu- <imamurasyunnsuke.patagonia@gmail.com>
2021-03-12 15:55:50 +0900

    no message

--- Assets/STeam/Script/Title.cs
+++ Assets/STeam/Script/Title.cs
@@ -5,18 +5,6 @@ using UnityEngine.SceneManagement;
 
 public class Title : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     public void SceneGameStart()//ゲームシーンに移動
     {
         SceneManager.LoadScene("STeam/Scenes/rouka");
