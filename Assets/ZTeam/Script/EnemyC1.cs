f4dee64b6d901b774a626c1ed3c4d1aeb91af51c
2023150-PC\2023150 <daigakukait2023@gmail.com>
2021-03-25 20:27:13 +0900

    所持金の増減

--- Assets/ZTeam/Script/EnemyC1.cs
+++ Assets/ZTeam/Script/EnemyC1.cs
@@ -10,7 +10,8 @@ public class EnemyC1 : MonoBehaviour
     [SerializeField]
     private int enemyArmorPoint;// 敵の体力の入れ物
 
-    GameObject gold;
+    GameObject Canvas;
+    Status Status;
     
     public ScanPlayer scanplayer;
     private bool isPlayerIn = false;//playerが範囲内にいるかどうか
@@ -19,15 +20,15 @@ public class EnemyC1 : MonoBehaviour
 
     private float timeOut=0.2f;
     private float timeElapsed;
-    private int Gold;
+   
 
     void Start()
     {
         // 敵の体力を初期化
         enemyArmorPoint = 3;
         enemybulletT= new GameObject("enemybullet").transform;
-        
-        
+        Status = GetComponent<Status>();
+
     }
 
     // 弾オブジェクトと接触したときに呼び出される関数
@@ -45,7 +46,10 @@ public class EnemyC1 : MonoBehaviour
             }
             else {
                 // 敵の体力が0になったら敵オブジェクトを消滅させる
-                gold = GetComponent<Status>();//
+                //
+                Canvas = GameObject.Find("Canvas");
+                Status = Canvas.GetComponent<Status>();
+                Status.statusG += 1;
                Destroy(gameObject);
             }
         }
