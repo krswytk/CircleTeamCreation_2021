ba4ab35a16f6427d7f7a169397da02dc93d8e00b
douguya-imamu- <imamurasyunnsuke.patagonia@gmail.com>
2021-04-23 17:02:31 +0900

    no message

--- Assets/STeam/Script/Title.cs
+++ Assets/STeam/Script/Title.cs
@@ -5,9 +5,23 @@ using UnityEngine.SceneManagement;
 
 public class Title : MonoBehaviour
 {
+    
+
     public void SceneGameStart()//ゲームシーンに移動
     {
         SceneManager.LoadScene("STeam/Scenes/rouka/East rouka");
     }
+
+
+
+
+
+
+
+
+
+
+
+
 
 }
