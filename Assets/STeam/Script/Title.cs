69a12dde5db9d2e48ef288f9b8c341d1f1aec80c
douguya-imamu- <imamurasyunnsuke.patagonia@gmail.com>
2021-04-30 17:48:06 +0900

    Zキー選択

--- Assets/STeam/Script/Title.cs
+++ Assets/STeam/Script/Title.cs
@@ -5,17 +5,44 @@ using UnityEngine.SceneManagement;
 
 public class Title : MonoBehaviour
 {
-    
+    int StertF = 0;//0:初期状態　1:チュートリアル　2：初めから　３：つづきから　４：遊び方
+    int A = 0;
 
-    public void SceneGameStart()//ゲームシーンに移動
+
+
+    private void Update()
     {
-        SceneManager.LoadScene("STeam/Scenes/rouka/East rouka");
+        Debug.LogError(StertF);
+
+
+        StertF = GameModeHAZIMEKARA();
+        if (Input.GetKeyDown(KeyCode.Z) && StertF == 1)
+        {
+            //zキーが押されたときの処理
+
+            Debug.LogError(3000);
+
+            SceneManager.LoadScene("STeam/Scenes/rouka/East rouka");
+
+            StertF = 0;
+        }
     }
 
 
+    
 
 
 
+    public int　GameModeHAZIMEKARA()//ゲームシーンに移動
+    {
+
+        StertF = 1;
+
+
+        return StertF;
+
+
+    }
 
 
 
