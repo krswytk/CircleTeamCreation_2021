29a7584de0410fdac361a96f0e1d69ca919a8612
numd-74 <74402384+numd-74@users.noreply.github.com>
2021-05-07 17:10:19 +0900

    弾の発射方向の改善

---
+++ Assets/ZTeam/Script/PlayerScript/lookAt.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class lookAt : MonoBehaviour
+{
+    public static bool lookright;
+    // Start is called before the first frame update
+    void Start()
+    {
+        lookright = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetAxis("Horizontal") != 0)
+        {
+            Lookright();
+        }
+    }
+    
+    bool Lookright()//現在どの方向を向いているかをBulletに返す
+    {
+        float horizontalKey = Input.GetAxis("Horizontal");
+        if (horizontalKey > 0) 
+        {
+            lookright = true;
+        }
+        else
+        {
+            lookright = false;
+        }
+        return lookright;
+    }
+
+}
