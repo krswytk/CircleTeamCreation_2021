89b539576597fe981a6a780b89d45f46cf8f24a6
1923036-PC\1923036 <s1923036@cco.kanagawa-it.ac.jp>
2021-05-10 22:29:52 +0900

    瓦礫と歩行

--- Assets/STeam/Script/PanelManager.cs
+++ Assets/STeam/Script/PanelManager.cs
@@ -5,8 +5,6 @@ using UnityEngine.UI;
 
 public class PanelManager : MonoBehaviour
 {
-    Panel Telop;
-    string itemtext;
     [SerializeField]
     GameObject Telopmanager;
 
@@ -15,7 +13,7 @@ public class PanelManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {   
-        Telop = Telopmanager.GetComponent<Panel>();
+
     }
 
     // Update is called once per frame
