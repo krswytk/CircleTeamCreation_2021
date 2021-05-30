5f6679517aa84676c3f13b9bbe66d0dbf1aefbaa
2023150-PC\2023150 <daigakukait2023@gmail.com>
2021-05-30 11:50:59 +0900

    敵の変更中

---
+++ Assets/ZTeam/Script/EnemyScript/EnemyC3b_s.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyC3b_s : MonoBehaviour
+{
+    Rigidbody2D rb;
+    GameObject Canvas;
+    Status Status;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        Canvas = GameObject.Find("Canvas");
+        Status = Canvas.GetComponent<Status>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+
+        if (collision.gameObject.tag == "Player")
+        {
+            Status.HP(-5);
+        }
+    }
+}
---
+++ Assets/ZTeam/Script/EnemyScript/EnemyC3r_s.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyC3r_s : MonoBehaviour {
+    private Transform _Transform;
+    private float Rotate;
+    
+
+    void Start() {
+
+        _Transform = GetComponent<Transform>();
+        Rotate = 0;
+
+    }
+    // Update is called once per frame 
+    void Update() {
+
+        Rotate = Rotate + 3;
+        _Transform.rotation = Quaternion.Euler(0, 0, Rotate);
+
+    }
+}
