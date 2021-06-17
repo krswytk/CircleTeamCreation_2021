ae12da0c11b9a7fe0b95a216c6565bb6eaa47cb7
yanagi121 <air_kent24@icloud.com>
2021-06-17 21:01:04 +0900

    ステージセレクトの変更

---
+++ Assets/ZTeam/Script/Stage_Select/RotateL_Center.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateL_Center : MonoBehaviour
+{
+ 
+    public GameObject CenterMarker;//= GameObject.Find("CenterMarker");
+    public GameObject stage1;
+    public GameObject stage2;
+    public GameObject Rrotate;
+    bool rotStart = false,rota=true;
+    float speed = 3.0f;
+    float rotAngle = 180f;
+    float variation;
+    float rot;
+    int  j = 1;
+    public static int RotateTotal=0;
+    //Start is called before the first frame update
+    void Start()
+    {
+        //CenterMarker = GameObject.Find("CenterMarker");
+
+        variation = rotAngle / speed;
+    }
+
+    //Update is called once per frame
+    void Update()
+    {
+        // Debug.Log(rotStart);
+        if (rotStart)
+        {
+            RotateTotal++;
+            CenterMarker.transform.Rotate(0, j, 0);
+            Debug.Log(RotateTotal);
+            Debug.Log(rotStart);
+            //rot += variation * Time.deltaTime;
+            if (RotateTotal % 180 == 0)
+            {
+                rotStart = false;
+                //rota = true;
+                Rrotate.SetActive(true);
+                //CenterMarker.transform.localRotation = Quaternion.Euler(0, 0, 0);
+            }
+        }
+        if (RotateTotal % 360 == 90)
+        {
+            stage1.SetActive(false);
+            stage2.SetActive(true);
+        }
+        if (RotateTotal % 360 == 270)
+        {
+            stage1.SetActive(true);
+            stage2.SetActive(false);
+        }
+        //Debug.Log(i);
+    }
+    public void OnClick()
+    {
+        // Debug.Log("押された!");
+        rot = 0f;
+        //CenterMarker.transform.localRotation = Quaternion.Euler(0, 0, 0);
+        rotStart = true;
+       // rota = false;
+        Rrotate.SetActive(false);
+        //this.gameObject.SetActive(false);
+    }
+}
+
---
+++ Assets/ZTeam/Script/Stage_Select/RotateR_Center.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateR_Center : MonoBehaviour
+{
+    public GameObject CenterMarker;//= GameObject.Find("CenterMarker");
+    public GameObject stage1;
+    public GameObject stage2;
+    public GameObject Lrotate;
+    bool rotStart = false;
+    float speed = 3.0f;
+    float rotAngle = 180f;
+    float variation;
+    float rot;
+    int i = 0,j=1;
+    //Start is called before the first frame update
+    void Start()
+    {
+        //CenterMarker = GameObject.Find("CenterMarker");
+
+        variation = rotAngle / speed;
+    }
+
+   //Update is called once per frame
+    void Update()
+    {
+      // Debug.Log(rotStart);
+        if (rotStart)
+        {
+            i++;
+            CenterMarker.transform.Rotate(0, -j, 0);
+            Debug.Log(RotateL_Center.RotateTotal++);
+            Debug.Log(rotStart);
+            //rot += variation * Time.deltaTime;
+            if (i %180==0)
+            {
+                rotStart = false;
+                Lrotate.SetActive(true);
+                //CenterMarker.transform.localRotation = Quaternion.Euler(0, 0, 0);
+            }
+        }
+        if(i%360==90)
+        {
+            stage1.SetActive(false);
+            stage2.SetActive(true);
+        }
+        if(i%360==270)
+        {
+            stage1.SetActive(true);
+            stage2.SetActive(false);
+        }
+        //Debug.Log(i);
+    }
+    public void OnClick()
+    {
+       // Debug.Log("押された!");
+       rot = 0f;
+       //CenterMarker.transform.localRotation = Quaternion.Euler(0, 0, 0);
+       rotStart = true;
+        Lrotate.SetActive(false);
+       //this.gameObject.SetActive(false);
+    }
+}
