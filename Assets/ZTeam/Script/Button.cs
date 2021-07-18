1b3bdb2e98e46b520ffa3e2a916cf28235c37ae7
2023150-PC\2023150 <daigakukait2023@gmail.com>
2021-07-18 14:13:14 +0900

    ゲームオーバーの後ろにパネルを配置　ポーションの購入画面を作成

---
+++ Assets/ZTeam/Script/Button.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Button : MonoBehaviour
+{
+    Status Status;
+    GameObject Canvas;
+    GameObject select;
+
+        private void Start()
+    {
+        select = GameObject.Find("select");
+        Canvas = GameObject.Find("Canvas");
+        Status = Canvas.GetComponent<Status>();
+    }
+    public void Onclick()
+    {
+        if (Status.statusG >= 25)
+        {
+            Status.GOLD(-25);
+        }
+        else
+        {
+            Debug.Log("購入できません");
+        }
+
+        select.gameObject.SetActive(false);
+
+    }
+
+    public void Onclick2()
+    {
+        select.gameObject.SetActive(false);
+    }
+}
