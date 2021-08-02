cffde026bbd52911536aed47bfdc84fcad22eb7f
2023150-PC\2023150 <daigakukait2023@gmail.com>
2021-08-02 15:48:13 +0900

    ボスの状態遷移設定　ボスの弾とスクリプト作成

---
+++ Assets/ZTeam/Script/EnemyScript/BossBulletS.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossBulletS : MonoBehaviour
+{
+
+    GameObject Canvas;//キャンバスを格納するオブジェクト
+    Status Status;//ステータススクリプトを格納する変数
+    Transform player;//プレイヤーの座標を格納する
+
+    GameObject Boss;
+    BossS BossS;
+    Vector3 BulletMovement;
+
+        // Start is called before the first frame update
+        void Start()
+    {
+        Canvas = GameObject.Find("Canvas");//キャンバスを検索
+        Status = Canvas.GetComponent<Status>();//キャンバスについているステータススクリプトを取得
+        player = GameObject.FindGameObjectWithTag("Player").transform;//プレイヤーをタグから取得
+        Boss = GameObject.Find("BOSS");//BOSSを検索
+        BossS = Canvas.GetComponent<BossS>();//BOSSについているスクリプトを取得
+
+        if (BossS.AttackStateNow == BossS.AttackState.Short)
+        {
+
+        }else if (BossS.AttackStateNow == BossS.AttackState.Middle)
+        {
+
+        }else if (BossS.AttackStateNow == BossS.AttackState.Long)
+        {
+
+        }
+        
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
