3bd1e39d30fc3bd5c758216c302089b0f04e54d7
numd-74 <74402384+numd-74@users.noreply.github.com>
2021-08-16 22:23:16 +0900

    チュートリアルステージのイメージ

---
+++ Assets/ZTeam/Script/Textchecker.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Textchecker : MonoBehaviour
+{
+    public GameObject back;
+    public GameObject text;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Player")
+        {
+            back.gameObject.SetActive(true);
+            text.gameObject.SetActive(true);
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            text.gameObject.SetActive(false);
+            back.gameObject.SetActive(false);
+        }
+    }
+}
