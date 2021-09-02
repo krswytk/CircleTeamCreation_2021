a2146bdd15bcb65a9a457d1bd96111fe76422a4c
numd-74 <74402384+numd-74@users.noreply.github.com>
2021-09-02 17:45:31 +0900

    チュートリアルver.2

---
+++ Assets/ZTeam/Script/Clear.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Clear : MonoBehaviour
+{
+    public GameObject tclear;
+    // Start is called before the first frame update
+    void Start()
+    {
+        tclear.gameObject.SetActive(false);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            tclear.gameObject.SetActive(true);
+        }
+    }
+
+}
